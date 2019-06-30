61c2654dcae6b8c6421325f035c39cae680540ed
CrunchyPancakes <30matthewt@gmail.com>
2019-06-30 13:23:59 +1200

    Styling adjustments

--- INFO264/website/script.js
+++ INFO264/website/script.js
@@ -4,8 +4,24 @@ window.onscroll = function() {scrollFunction()}; // set an event listener that d
 // scrollTop returns the number of pixels that an element has been scrolled vertically. In our case, the scroll top of the window starts at 0, and increases as the user scrolls down.
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("logo").style.fontSize = "25px";
+    document.getElementById("logo").style.fontSize = "28px";
+    document.getElementById("navbar").style.backgroundColor = "#272727";
+
+    var x = document.getElementById("navbar"); // stores the navbar element inside a variable
+    var y = x.getElementsByTagName("a"); // accesses the a tags within the navbar, and store them in an array
+    var i; // for each a tag within the array, do something to it
+    for(i = 0; i < y.length; i++){
+      y[i].style.color = "#009fb7";
+    }
   } else {
-    document.getElementById("logo").style.fontSize = "35px";
+    document.getElementById("logo").style.fontSize = "30px";
+    document.getElementById("navbar").style.backgroundColor = "#009fb7";
+
+    var x = document.getElementById("navbar"); // stores the navbar element inside a variable
+    var y = x.getElementsByTagName("a"); // accesses the a tags within the navbar, and store them in an array
+    var i; // for each a tag within the array, do something to it
+    for(i = 0; i < y.length; i++){
+      y[i].style.color = "#201d1e";
+    }
   }
 }
